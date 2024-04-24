c930c052cfefe7b4be71449866aedf56324f8b97
Rigoberto Cepeda <rcepeda@infracommerce.lat>
2024-04-23 18:00:11 -0600

    fix(UI): :lipstick: Fixed css ui

--- src/components/ui/Table/index.tsx
+++ src/components/ui/Table/index.tsx
@@ -10,7 +10,7 @@ export const Table = ({ table }: ITable) => {
 		<table className={styles.table}>
 			<thead className={styles.table_head}>
 				<tr>
-					<th className={styles.table_head_club}>Club</th>
+					<th className={`${styles.table_head_club} ${styles.table_item_sticky}`}>Club</th>
 					<th>MG</th>
 					<th>MW</th>
 					<th>MD</th>
