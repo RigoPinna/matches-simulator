c30458d5a58c21e66f21dde6a64f20f2b794e027
Rigoberto Cepeda <riguxc@gmail.com>
2025-04-09 17:35:22 -0600

    feat(club): add ClubPage with statistics and last matches display

---
+++ src/hooks/useStatistics.ts
@@ -0,0 +1,46 @@
+import { useContext, useEffect, useState } from "react";
+import { SeassonContext } from "../store";
+
+
+export const useStatistics = (cid: string) => {
+  const { seasons } = useContext(SeassonContext);
+
+  const [statics, setStatics] = useState({
+    wins: 0,
+    draws: 0,
+    losses: 0,
+    winPercentage: 0,
+    matchesDone: 0
+  })
+
+  useEffect(() => {
+    const allMatches = seasons.map(season => [
+      ...season.fase.regular.matches.matches,
+      ...season.fase.semifinal.matches.matches,
+      ...season.fase.final.matches.matches]).flat()
+    const matchesDone = allMatches.filter(match => match.status === 'DONE');
+    const matches = matchesDone.map(match => match.value).flat();
+    const matchesByClub = matches.filter(match => match.local.uuid === cid || match.visit.uuid === cid)
+    let wins = 0, draws = 0, losses = 0;
+    for (const match of matchesByClub) {
+      if (typeof match.local.score === 'number' && typeof match.visit.score === 'number') {
+        if (match.local.score === match.visit.score) {
+          draws++
+        } else {
+          const winner = (match.local.score > match.visit.score) ? match.local.uuid : match.visit.uuid;
+
+          (winner === cid) ? wins++ : losses++;
+        }
+
+      }
+    }
+    setStatics({
+      wins,
+      draws,
+      losses,
+      winPercentage: wins / matchesByClub.length * 100,
+      matchesDone: matchesByClub.length
+    })
+  }, [])
+  return statics
+}
---
+++ src/pages/Club/index.tsx
@@ -0,0 +1,62 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { ArrowLeft } from "../../components/icons";
+import { Main } from "../../components/layouts";
+import { Button, Header, LastMatches } from "../../components/ui";
+import { useContext } from "react";
+import { SeassonContext } from "../../store";
+import lige from '../../assets/champion.png'
+import supercup from '../../assets/supercup.png'
+import styles from "./styles.module.css";
+import { useStatistics } from "../../hooks/useStatistics";
+export const ClubPage = () => {
+    const navigate = useNavigate();
+    const { cid } = useParams();
+    const { clubs, seasons } = useContext(SeassonContext);
+    const statistics = useStatistics(cid || '')
+    const club = clubs.find(({ uuid }) => uuid === cid);
+    if (!club) {
+        return <div>Club not found</div>;
+    }
+    return (
+        <>
+            <Header>
+                <Button.Secondary
+                    onClick={() => navigate(-1)}>
+                    <ArrowLeft />
+                    Back
+                </Button.Secondary>
+                <h1>Club</h1>
+            </Header>
+            <Main>
+                <div className={styles.header}>
+                    <img src={club?.image} alt={club?.name} />
+                    <h2>{club?.name}</h2>
+                    <LastMatches showtitles clubId={club.uuid} seasonId={seasons[seasons.length - 1].uuid} />
+
+                </div>
+                <ul className={styles.list}>
+                    <li className={styles.list_item}>
+                        <p className={styles.number_trophy}>{club.champions}</p>
+                        <h3 className={styles.title}>Leagues</h3>
+                        <img src={lige} alt="champion" className={styles.champ} />
+                    </li>
+                    <li className={styles.list_item}>
+                        <p className={styles.number_trophy}>{club.supercups}</p>
+                        <h3 className={styles.title}>Supercups</h3>
+                        <img src={supercup} alt="champion" className={styles.champ} />
+                    </li>
+                    <li className={styles.list_item}>
+                        <p className={styles.number_trophy}>{statistics.winPercentage.toFixed(1)}%</p>
+                        <div className={styles.statistic_wrapper}>
+                            <div className={styles.statistic}><p>{statistics.wins}</p>W</div>
+                            <div className={styles.statistic}><p>{statistics.draws}</p>D</div>
+                            <div className={styles.statistic}><p>{statistics.losses}</p>L</div>
+
+                        </div>
+                        <h3 className={styles.title}>Winning percentage</h3>
+                    </li>
+                </ul>
+            </Main>
+        </>
+    )
+}
